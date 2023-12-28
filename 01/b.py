fb61c57569f554665ae0ff0a7343cfc4939c72b2
Nick Muggio <nick@mugg.io>
2023-12-28 18:09:51 -0500

    day 1

---
+++ 01/b.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import sys
+
+with open(sys.argv[1], mode="rt", encoding="utf-8") as f:
+  line = f.readline().strip()
+  total = 0
+  line2 = line[len(line)//2:] + line[:len(line)//2]
+  for i in range(len(line)):
+    if line[i] == line2[i]:
+      total += ord(line[i]) - ord('0')
+print(total)
